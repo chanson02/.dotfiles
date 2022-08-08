bf7f829778c4f731c3f71fa77feadfa4469636b6
Cooper Hanson <cooperhanson2020@gmail.com>
2022-08-07 22:56:46 -0500

    fixed some bugs with plugin installations

--- lua/chanson/treesitter.lua
+++ lua/chanson/treesitter.lua
@@ -5,7 +5,7 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = { 'ruby', 'javascript' },
+  --ensure_installed = { 'ruby', 'javascript' },
   highlight = { enable = true },
   auto_install = true, -- Automatically install parsers for files I use
 
--- nvim.sh
+++ nvim.sh
@@ -2,7 +2,7 @@
 
 # Dependencies
 apt install curl gcc g++ -y
-apt-get remove neovim -y # delete old nvim if installed
+apt-get --purge remove neovim -y # delete old nvim if installed
 
 # This link redirects to the newest version of nvim
 nvim_redirect='https://github.com/neovim/neovim/releases/latest'
@@ -18,13 +18,17 @@ curl -L $nvim_download -o $nvim_installer
 dpkg -i ./$nvim_installer
 apt-get install -f -y
 rm $nvim_installer
+apt autoremove
 
 # Create nvim config
 home=$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6) # home dir
-mkdir -p $home/.config/nvim
-#TO-DO: Delete /rename old files?
+nvim_home="$home/.config/nvim/plugin"
+mkdir -p $nvim_home
+if [ -f "$nvim_home/init.lua" ] ; then rm $nvim_home/init.lua; fi
+if [ -d "$nvim_home/lua"]; then rm -Rf $nvim_home/lua; fi
+
 ln -s $home/.dotfiles/init.lua $home/.config/nvim/init.lua
 ln -s $home/.dotfiles/lua $home/.config/nvim/lua
 
-# Install plugins
-nvim -c "q" $home/.dotfiles/lua/chanson/plugins.lua
+sudo -u $SUDO_USER nvim -c "q" # Install packer
+sudo -u $SUDO_USER nvim -c "w" $home/.dotfiles/lua/chanson/plugins.lua # Install plugins
