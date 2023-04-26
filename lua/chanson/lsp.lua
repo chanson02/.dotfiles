d55e73cd213d330e74d64c88ba82920ec109bafb
Cooper Hanson <hancooj@dunwoody.edu>
2023-04-26 09:49:48 -0500

    pressing enter in cmp doesnt go to new line

--- lua/chanson/lsp.lua
+++ lua/chanson/lsp.lua
@@ -24,8 +24,8 @@ cmp.setup({
 --    ['<Tab>'] = cmp_action.tab_complete(),
 --    ['<S-Tab>'] = cmp_action.select_prev_or_fallback()
     ['<Tab>'] = cmp_action.luasnip_supertab(),
-    ['<S-Tab'] = cmp_action.luasnip_shift_supertab()
-
+    ['<S-Tab'] = cmp_action.luasnip_shift_supertab(),
+    ['<CR>'] = cmp.mapping.confirm({select = false})
   }
 })
 
