45ed5c377ea7853472fdf193b72480f2077a8413
Cooper Hanson <hancooj@dunwoody.edu>
2023-05-22 19:17:02 -0500

    Updated quickscope colors

--- lua/chanson/quickscope.lua
+++ lua/chanson/quickscope.lua
@@ -1,5 +1,7 @@
 -- Requires unblevable/quick-scope
 
+vim.cmd('highlight QuickScopePrimary ctermfg=yellow guifg=yellow')
+vim.cmd('highlight QuickScopeSecondary ctermfg=cyan guifg=cyan')
 vim.cmd [[
 let g:qs_highlight_on_keys = ['f', 'F', 't', 'T'] " Only highlight when in search mode
 ]]
