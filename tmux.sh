928c3b3323057a9acb38419309fa935d05b755b6
Cooper Hanson <hancooj@dunwoody.edu>
2023-05-26 11:14:34 -0500

    Install tmux plugins

--- tmux.sh
+++ tmux.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
 home=$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6) # home dir
 
 apt-get update
 apt-get upgrade
-apt-get install git
-apt-get install tmux
+apt-get install -y git tmux
 
 git clone https://github.com/tmux-plugins/tmp $home/.tmux/plugins/tpm
 
 mkdir -p $home/.config/tmux
 ln -s $home/.dotfiles/tmux.conf $home/.tmux.conf
+
+# install plugins
+tmux new-session -d
+bash $home/.tmux/plugins/tpm/bin/install_plugins
+tmux kill-session -t 0
