03f3269bf6a98c9f5043ebc971e926f6451b0c1e
Cooper Hanson <hancooj@dunwoody.edu>
2023-05-26 11:24:02 -0500

    Moved rbenv to bash_vars.sh

--- rails.sh
+++ rails.sh
@@ -1,21 +1,18 @@
 home=$(getent passwd ${SUDO_USER:-$USER} | cut -d: -f6) # home dir
 
+apt-get update
+apt-get install -y git curl libssl-dev libreadline-dev zlib1g-dev
+
 # Download rbenv
 git clone https://github.com/rbenv/rbenv.git $home/.rbenv
 chmod -R 777 $home/.rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $home/.bashrc
-echo 'eval "$(rbenv init -)"' >> $home/.bashrc
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> $home/.dotfiles/bash_vars.sh
+echo 'eval "$(rbenv init -)"' >> $home/.dotfiles/bash_vars.sh
+source $home/.bashrc
 
 # Download ruby installer
 git clone https://github.com/rbenv/ruby-build.git $home/.rbenv/plugins/ruby-build
-echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> $home/.bashrc
-
-
-### THE REST OF THIS DOES NOT WORK
-# Don't wait for shell restart
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> $home/.dotfiles/bash_vars.sh
 
 # install latest ruby/rails versions
 latest=$(rbenv install -l | grep -v - | tail -1)
