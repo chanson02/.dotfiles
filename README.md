d2f6c91492de6a07ca0d254f968f7eee66225b56
Cooper <cooperhanson2020@gmail.com>
2023-11-08 11:06:54 -0600

    am going to remake everything from scratch with lazy

--- README.md
+++
@@ -1,3 +0,0 @@
-# chmod +x ./install.sh
-
-To install nvim run sudo bash nvim.sh
--- lua/chanson/quickscope.lua
+++
@@ -1,7 +0,0 @@
--- Requires unblevable/quick-scope
-
-vim.cmd('highlight QuickScopePrimary ctermfg=yellow guifg=yellow')
-vim.cmd('highlight QuickScopeSecondary ctermfg=cyan guifg=cyan')
-vim.cmd [[
-let g:qs_highlight_on_keys = ['f', 'F', 't', 'T'] " Only highlight when in search mode
-]]
