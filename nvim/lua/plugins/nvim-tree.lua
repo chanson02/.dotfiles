af0ca6184a7f7a390732fd9382daff2edacfa871
Cooper <cooperhanson2020@gmail.com>
2023-11-08 15:52:21 -0600

    added Nvim tree for file navigation

---
+++ nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,18 @@
+local config = function()
+  local nvimtree = require('nvim-tree')
+  nvimtree.setup({
+    disable_netrw = true,
+    auto_reload_on_write = true,
+    git = { ignore = false }, --show git ignored files
+    view = { number = true }, -- use line numbers to jump around
+    renderer = { group_empty = true }, -- Folders with only one item: show item instead of folder
+    actions = { open_file = { quit_on_open = true } } -- close explorer when file is opened
+  })
+
+  vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>')
+end
+return {
+  'nvim-tree/nvim-tree.lua',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = config
+}
