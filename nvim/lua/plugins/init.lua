ca67aa3433ced0076b158557eb54cb60cfe7d2b3
Cooper <cooperhanson2020@gmail.com>
2023-11-09 17:20:38 -0600

    removed tmux in favor of zellij

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,6 +1,4 @@
 return {
-  'christoomey/vim-tmux-navigator', -- navigate between vim and tmux panes
-
   { 'stevearc/dressing.nvim', event = 'VeryLazy' }, -- UI plugin to change how neovim gathers user input
 
   { 'tpope/vim-fugitive', event = 'VeryLazy' }, -- Plugin to interact with git without leaving the buffer!
