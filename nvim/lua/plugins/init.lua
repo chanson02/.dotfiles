795a1ca2aac297ecaa6ff9284ed573d143a1f762
Cooper <cooperhanson2020@gmail.com>
2023-11-09 15:51:34 -0600

    Added vim fugitive

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,5 +1,7 @@
 return {
   'christoomey/vim-tmux-navigator', -- navigate between vim and tmux panes
 
-  { 'stevearc/dressing.nvim', event = 'VeryLazy' } -- UI plugin to change how neovim gathers user input
+  { 'stevearc/dressing.nvim', event = 'VeryLazy' }, -- UI plugin to change how neovim gathers user input
+
+  { 'tpope/vim-fugitive', event = 'VeryLazy' }, -- Plugin to interact with git without leaving the buffer!
 }
