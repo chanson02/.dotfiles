893dc382ec84d5bb46f56ecb1bb43b28c92e7238
Cooper <cooperhanson2020@gmail.com>
2023-11-09 11:01:08 -0600

    fixed an error where the keymaps for lsp were set for all buffers

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -9,30 +9,29 @@ local deps = {
 
 -- function runs when LSP client attaches to buffer
 local on_attach = function(_, bufnr)
-  local keymap = vim.api.nvim_set_keymap
+  local keymap = vim.api.nvim_buf_set_keymap
   local opts = { noremap = true, silent = true, desc = "" }
-  opts.buffer = bufnr
 
   opts.desc = 'Search LSP references'
-  keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', opts) -- references are where class methods are used
+  keymap(bufnr, 'n', 'gr', '<cmd>Telescope lsp_references<CR>', opts) -- references are where class methods are used
 
   opts.desc = 'Search LSP implementations'
-  keymap('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts) -- implementations are where classes are used
+  keymap(bufnr, 'n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts) -- implementations are where classes are used
 
   opts.desc = 'Go to definition'
-  keymap('n', 'gd', vim.lsp.buf.definition.opts)
+  keymap(bufnr, 'n', 'gd', vim.lsp.buf.definition.opts)
 
   opts.desc = 'Go to type definition'
-  keymap('n', 'gt', vim.lsp.buf.type_definition, opts)
+  keymap(bufnr, 'n', 'gt', vim.lsp.buf.type_definition, opts)
 
   opts.desc = 'See code actions'
-  keymap({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
+  keymap(bufnr, {'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
 
   opts.desc = 'Smart rename'
-  keymap('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  keymap(bufnr, 'n', '<leader>rn', vim.lsp.buf.rename, opts)
 
   opts.desc = 'Documentation'
-  keymap('n', 'K', vim.lsp.buf.hover, opts)
+  keymap(bufnr, 'n', 'K', vim.lsp.buf.hover, opts)
 end
 
 
@@ -47,7 +46,7 @@ local config = function()
   local default_handler = function(server)
     lsp[server].setup({
       capabilities = caps,
-      on_attach = on_attach
+      on_attach = on_attach,
     })
   end
 
