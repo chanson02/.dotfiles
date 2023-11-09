88b1df2ac8f0babe4a096373c0ec22c9aff44eae
Cooper <cooperhanson2020@gmail.com>
2023-11-09 16:02:56 -0600

    Added something to come back for

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,4 +4,5 @@ return {
   { 'stevearc/dressing.nvim', event = 'VeryLazy' }, -- UI plugin to change how neovim gathers user input
 
   { 'tpope/vim-fugitive', event = 'VeryLazy' }, -- Plugin to interact with git without leaving the buffer!
+  -- { 'akinsho/git-conflict.nvim', event = 'VeryLazy' }, -- I really want to try this in the future
 }
