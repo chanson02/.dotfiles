f615b7a3ddb2c4cca360b6c971f2ea4a61274455
Cooper <cooperhanson2020@gmail.com>
2023-11-09 15:14:47 -0600

    added treesitter

---
+++ nvim/lua/plugins/lsp/treesitter.lua
@@ -0,0 +1,24 @@
+--[[
+This isn't exactly an LSP extension, but I have it working with the LSP
+Tree Sitter takes in a language and some source code and can parse out the meaning of the queries
+It's most famous use case is highlighting and text colors
+]]
+
+local config = function()
+  local ts = require('nvim-treesitter.configs')
+
+  ts.setup({
+    highlight = { enabled = true },
+    indent = { enabled = true }, -- Make smarter indentations using parser
+    auto_install = true, -- install parsers when file is opened
+    additional_vim_regex_highlighting = false, -- I've had issues with the default highlighting
+  })
+end
+
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  depdendencies = { 'nvim-treesitter/nvim-treesitter-textobjects' }, -- configured in it's own file
+  config = config,
+  event = { 'BufReadPre', 'BufNewfile' }
+}
