b7ff3127ba12bf3f5062d6691ebe28da7719de1a
Cooper <cooperhanson2020@gmail.com>
2023-11-09 20:14:26 -0600

    Create symlinks for dotfiles

--- scripts/install_zellij.sh
+++ scripts/install_zellij.sh
@@ -1,6 +1,14 @@
-#1/bin/bash
+#!/bin/bash
+
+scripts_dir="$( cd "$( dirname "$BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+dfiles_dir="$(dirname $scripts_dir)"
 
 if command -v dnf &> /dev/null; then
   sudo dnf copr enable varlad/zellij
 fi
 bash "$scripts_dir/install_package.sh" "zellij"
+
+zell_home="$HOME/.config/zellij"
+mkdir -p $zell_home
+rm -rf $zell_home # remove old zellig
+ln -s "$dfiles_dir/zellij" "$zell_home"
