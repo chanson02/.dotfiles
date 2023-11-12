7d73b01ce70d7bb72691e1b828f45715578f4d0a
Cooper Hanson <cooperhanson2020@gmail.com>
2023-11-12 15:48:41 -0600

    Lazy load quickscope

--- nvim/lua/plugins/quickscope.lua
+++ nvim/lua/plugins/quickscope.lua
@@ -1,5 +1,6 @@
 return {
   'unblevable/quick-scope',
+  keys = { 'f', 'F', 't', 'T' },
   init = function()
     vim.cmd [[ let g:qs_highlight_on_keys = ['f', 'F', 't', 'T'] ]]
     vim.cmd [[ let g:qs_max_chars=150 ]]
