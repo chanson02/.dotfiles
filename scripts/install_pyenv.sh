23a0c9a54321e2d6d5ab5940cf10a63b07f9a7da
Cooper Hanson <cooperhanson2020@gmail.com>
2023-11-12 14:57:29 -0600

    Add a script for installing pyenv

---
+++ scripts/install_pyenv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl "https://pyenv.run" | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+
