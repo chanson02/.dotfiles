925023c7ca8085e7a535c39d53c6ee11212fcad6
Cooper Hanson <cooperhanson2020@gmail.com>
2023-11-12 16:52:49 -0600

    Install the proper packages for postgres

--- scripts/install_rbenv.sh
+++ scripts/install_rbenv.sh
@@ -5,6 +5,9 @@ declare -a dependencies=(
   'rbenv'
   'postgresql'
 )
+
 bash "$scripts_dir/install_package.sh" "${dependencies[@]}"
+(bash "$scripts_dir/install_package.sh" "libpq-dev") || true # debian
+(bash "$scripts_dir/install_package.sh" "postgresql-devel") || true # RHEL
 sudo service postgresql start
 echo 'eval "$(rbenv init - bash)"' >> ~/.bash_profile
