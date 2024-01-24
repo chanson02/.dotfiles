858af134430c97132ab25d0036eb3de8e173b8b4
Cooper <cooperhanson2020@gmail.com>
2024-01-24 17:27:37 -0600

    updated pyenv to use bashrc instead of profile

--- scripts/install_pyenv.sh
+++ scripts/install_pyenv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+rm -rf $HOME/.pyenv
 curl "https://pyenv.run" | bash
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bash_profile
-
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
