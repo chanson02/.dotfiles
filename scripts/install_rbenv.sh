fc581b4f96d5eec7728fecc5bfeac60ab98810b1
Cooper <cooperhanson2020@gmail.com>
2024-01-25 11:13:52 -0600

    switch to latest rbenv version

--- scripts/install_rbenv.sh
+++ scripts/install_rbenv.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 scripts_dir="$( cd "$( dirname "$BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-declare -a dependencies=(
-  'rbenv'
-  'postgresql'
-)
-
-bash "$scripts_dir/install_package.sh" "${dependencies[@]}"
+bash "$scripts_dir/install_package.sh" "postgresql"
 (bash "$scripts_dir/install_package.sh" "libpq-dev") || true # debian
 (bash "$scripts_dir/install_package.sh" "postgresql-devel") || true # RHEL
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'eval "$(~/.rbenv/bin/rbenv init - bash)"' >> ~/.bashrc
+eval "$(~/.rbenv/bin/rbenv init - bash)"
+
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+
+echo '!!! Restart shell to finsih rbenv install'
 sudo service postgresql start
-echo 'eval "$(rbenv init - bash)"' >> ~/.bash_profile
