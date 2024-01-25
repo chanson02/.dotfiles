e63551c409431399a229ce0f37dc4c6cbe3d4caa
Cooper <cooperhanson2020@gmail.com>
2024-01-25 10:31:39 -0600

    install curl as pyenv dep

--- scripts/install_pyenv.sh
+++ scripts/install_pyenv.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+scripts_dir="$( cd "$( dirname "$BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+bash "$scripts_dir/install_package.sh" "curl"
+
 rm -rf $HOME/.pyenv
 curl "https://pyenv.run" | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
@@ -7,4 +10,4 @@ echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bas
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 
-echo '!!! Restart Shell for Pyenv to finsih installing'
+echo "!!! Restart Shell for Pyenv to finsih installing"
