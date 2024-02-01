fcc29d1f73e20276a6abaddf4a5dc237fa99dacb
Cooper Hanson <cooperhanson2020@gmail.com>
2024-02-01 10:39:10 -0600

    updated install_package to not fail if package not found

--- scripts/install_package.sh
+++ scripts/install_package.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-echo "The deps are $@"
+install_with() {
+  local manager="$1"
+  local package="$2"
+  sudo $manager install -y $package || true
+}
+
 if [ -x "$(command -v apt)" ]; then
-  output=$(sudo apt install -y "$@" 2>&1)
+  manager="apt"
 elif [ -x "$(command -v dnf)" ]; then
-  output=$(sudo dnf install -y "$@" 2>&1)
+  manager="dnf"
 else
-  echo "unexpected package manager"
+  echo "Unsupported package manager"
   exit 1
 fi
 
-if echo "$output" | grep -q "but there is a snap with that name"; then
-  sudo snap install "$@" --classic
-fi
+packages=( "$@" )
+for package in "${packages[@]}"; do
+  install_with $manager "$package"
+done
