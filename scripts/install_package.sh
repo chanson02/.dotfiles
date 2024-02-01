f1ebe004bc07e07654d47a6a158a185a281d18d6
Cooper Hanson <cooperhanson2020@gmail.com>
2024-02-01 11:16:12 -0600

    Adjust package name for dnf

--- scripts/install_package.sh
+++ scripts/install_package.sh
@@ -3,11 +3,21 @@
 install_with() {
   local manager="$1"
   local package="$2"
+
+  # adjust package name for dnf
+  if [ "$manager" == "dnf" ] && [[ "$package" == *"-dev" ]]; then
+    package="${package/-dev/-devel}"
+  fi
+
+  echo "Installing $package with $manager..."
   output=$(sudo $manager install -y $package 2>&1)
-  echo $output
+  
+  local exit_code=$?
 
   if echo "$output" | grep -q "but thre is a snap with that name"; then
     sudo snap install "$@" --classic
+  elif [ $exit_code -ne 0 ]; then
+    echo "WARN: $output"
   fi
 }
 
