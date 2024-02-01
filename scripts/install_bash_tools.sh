25467743c53f9861201cb4dbc0b614deb2f2351c
Cooper Hanson <cooperhanson2020@gmail.com>
2024-01-31 21:27:05 -0600

    Install z.sh with bashtools

--- scripts/install_bash_tools.sh
+++ scripts/install_bash_tools.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Install z
+git clone https://github.com/rupa/z/ ~/tmp/z
+chmod +x ~/tmp/z/z.sh
+sudo mv ~/tmp/z/z.sh /usr/local/bin/
+sudo mv ~/tmp/z/z.1 /usr/local/share/man/man1
+rm -rf ~/tmp/z
+echo ". /usr/local/bin/z.sh" >> ~/.bashrc
+
+
 scripts_dir="$( cd "$( dirname "$BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 tools="$(dirname $scripts_dir)/bash_tools/init.sh"
 grep -qxF "source $tools" ~/.bashrc || echo "source $tools" >> ~/.bashrc
