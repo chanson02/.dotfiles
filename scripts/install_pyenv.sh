a6a152ebd473e82eb3a29d7ee14e907a29d8c25b
Cooper Hanson <cooperhanson2020@gmail.com>
2024-03-07 14:12:15 -0600

    automatically install a python version

--- scripts/install_pyenv.sh
+++ scripts/install_pyenv.sh
@@ -3,6 +3,7 @@
 scripts_dir="$( cd "$( dirname "$BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 declare -a dependencies=(
   'curl'
+  'gcc'
   'libbz2-dev'
   'zlib-devel'
   'bzip2-devel'
@@ -18,10 +19,14 @@ declare -a dependencies=(
 bash "$scripts_dir/install_package.sh" "${dependencies[@]}"
 
 rm -rf $HOME/.pyenv
-curl "https://pyenv.run" | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 
-echo "!!! Restart Shell for Pyenv to finsih installing"
+curl "https://pyenv.run" | bash
+source ~/.bashrc
+
+version=$(pyenv install --list | grep -v '[a-zA-Z]' | tail -n 1)
+pyenv install $version
+pyenv global $version
