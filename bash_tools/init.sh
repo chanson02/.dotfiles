68ad7ec4eefe819d3771ececbfae72eedd2e2806
Cooper Hanson <cooperhanson2020@gmail.com>
2024-06-07 11:56:21 -0500

    Updated how the bash_tools is going to work

--- bash_tools/init.sh
+++
@@ -1,5 +0,0 @@
-# vim mode
-set -o vi
-
-alias nocors='google-chrome --disable-web-security --disable-gpu --user-data-dir=~/chromeTemp'
-
--- scripts/install_bash_tools.sh
+++ scripts/install_bash_tools.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+add_to_bashrc() {
+  local line="$1"
+  if ! grep -Fxq "$line" ~/.bashrc; then
+    echo "$line" >> ~/.bashrc
+  fi
+}
+add_to_bashrc "set -o vi # vim mode"
+add_to_bashrc "alias nocors='google-chrome --disable-web-security --disable-gpu --user-data-dir=$HOME/chromeTemp'"
+
 # Install z
 git clone https://github.com/rupa/z/ ~/tmp/z
 chmod +x ~/tmp/z/z.sh
 sudo mv ~/tmp/z/z.sh /usr/local/bin/
 sudo mv ~/tmp/z/z.1 /usr/local/share/man/man1
 rm -rf ~/tmp/z
-echo ". /usr/local/bin/z.sh" >> ~/.bashrc
+add_to_bashrc ". /usr/local/bin/z.sh"
 . /usr/local/bin/z.sh # start z
 
-
 scripts_dir="$( cd "$( dirname "$BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-tools="$(dirname $scripts_dir)/bash_tools/init.sh"
-grep -qxF "source $tools" ~/.bashrc || echo "source $tools" >> ~/.bashrc
+tools_dir="$(dirname $scripts_dir)/bash_tools"
+target="/usr/local/bin/bash_tools"
+
+if [ ! -d "$target" ]; then
+  sudo ln -s "$tools_dir" "$target"
+fi
