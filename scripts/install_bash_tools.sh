fbb946a92f1292f1089b367deb8bb30db4fefd05
Cooper Hanson <cooperhanson2020@gmail.com>
2024-06-13 14:28:51 -0500

    Added a bash_tool for inserting into bashrc

--- scripts/install_bash_tools.sh
+++ scripts/install_bash_tools.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-add_to_bashrc() {
-  local line="$1"
-  if ! grep -Fxq "$line" ~/.bashrc; then
-    echo "$line" >> ~/.bashrc
-  fi
-}
-add_to_bashrc "set -o vi # vim mode"
-add_to_bashrc "alias nocors='google-chrome --disable-web-security --disable-gpu --user-data-dir=$HOME/chromeTemp'"
+scripts_dir="$( cd "$( dirname "$BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+target="$(dirname $scripts_dir)/bash_tools.sh"
+. $target
+bash_tools add_to_bashrc ". $target"
+
+bash_tools add_to_bashrc "set -o vi # vim mode"
+bash_tools add_to_bashrc "alias nocors='google-chrome --disable-web-security --disable-gpu --user-data-dir=$HOME/chromeTemp'"
 
 # Install z
 git clone https://github.com/rupa/z/ ~/tmp/z
@@ -15,9 +14,5 @@ chmod +x ~/tmp/z/z.sh
 sudo mv ~/tmp/z/z.sh /usr/local/bin/
 sudo mv ~/tmp/z/z.1 /usr/local/share/man/man1
 rm -rf ~/tmp/z
-add_to_bashrc ". /usr/local/bin/z.sh"
+bash_tools add_to_bashrc ". /usr/local/bin/z.sh"
 . /usr/local/bin/z.sh # start z
-
-scripts_dir="$( cd "$( dirname "$BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-target="$(dirname $scripts_dir)/bash_tools.sh"
-add_to_bashrc ". $target"
