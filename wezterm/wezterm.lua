6d6214b726f5d3bfe144596b414695641ca79415
Cooper Hanson <cooperhanson2020@gmail.com>
2024-07-23 09:41:31 -0500

    Add a background gradient to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -19,5 +19,14 @@ end
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 1
 config.font = wezterm.font_with_fallback({'MesloLGL Nerd Font Mono'})
+config.window_background_gradient = {
+  orientation = { Linear = { angle = 25.0 } },
+  colors = {
+    '#0f0c29',
+    '#302b63',
+    '#24243e',
+  },
+  interpolation = 'CatmullRom',
+}
 
 return config
