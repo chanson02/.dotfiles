585d9f453b8a2fcac31bf455dd27b297c5669aa8
Cooper Hanson <cooperhanson2020@gmail.com>
2024-08-07 09:34:25 -0500

    Added a markdown rendering plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -4,4 +4,12 @@ return {
   { 'tpope/vim-fugitive', event = 'VeryLazy' }, -- Plugin to interact with git without leaving the buffer!
   -- { 'akinsho/git-conflict.nvim', event = 'VeryLazy' }, -- I really want to try this in the future
 
+  {
+    'OXY2DEV/markview.nvim',
+    ft = 'markdown',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons'
+    }
+  },
 }
