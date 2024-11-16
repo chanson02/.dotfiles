66dc0ca82b82b44b70f5d73a9b3e39ab4c820d6e
Cooper Hanson <cooperhanson2020@gmail.com>
2024-11-15 21:50:51 -0600

    Add statusbar instance name

--- statusbar/app.ts
+++ statusbar/app.ts
@@ -3,6 +3,7 @@ import style from "./style.scss"
 import Bar from "./widget/Bar"
 
 App.start({
+    instanceName: 'chanson02-statusbar',
     css: style,
     main() {
         App.get_monitors().map(Bar)
