bae395dc8a7ccab5835e485fc3a57d67c9b8b841
Cooper Hanson <cooperhanson2020@gmail.com>
2024-11-15 23:09:43 -0600

    ags: Make hyprspaces a bit more readable

--- statusbar/widget/Hyprspaces.tsx
+++ statusbar/widget/Hyprspaces.tsx
@@ -1,24 +1,28 @@
 import Hyprland from "gi://AstalHyprland";
 import { bind } from "astal";
 
+const WorkspaceButton = ({ workspace, focusedSpace }) => {
+  const cls = workspace === focusedSpace ? "focused" : "";
+  return (
+    <button className={cls} onClicked={() => workspace.focus()}>
+      {workspace.id}
+    </button>
+  );
+};
+
 export default function Hyprspaces() {
   const hypr = Hyprland.get_default();
+  const focused = bind(hypr, "focusedWorkspace");
+  const spaces = bind(hypr, "workspaces").as((ws) =>
+    ws.sort((a, b) => a.id - b.id),
+  );
 
   return (
     <box className="Workspaces">
-      {bind(hypr, "workspaces").as((ws) =>
-        ws
-          .sort((a, b) => a.id - b.id)
-          .map((ws) => (
-            <button
-              className={bind(hypr, "focusedWorkspace").as((fw) =>
-                ws === fw ? "focused" : "",
-              )}
-              onClicked={() => ws.focus()}
-            >
-              {ws.id}
-            </button>
-          )),
+      {spaces.as((ws) =>
+        ws.map((ws) => (
+          <WorkspaceButton workspace={ws} focusedSpace={focused} />
+        )),
       )}
     </box>
   );
