8a3639f8270756d6c53a8e3cbbd6df7026e69f71
Cooper Hanson <cooperhanson2020@gmail.com>
2024-12-26 15:34:45 -0600

    Finally disabled change detection in lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,11 +19,9 @@ end
 -- put lazy into the vim's runtime path
 vim.opt.rtp:prepend(lazypath)
 
-
+require('lazy').setup('plugins', { change_detection = { enabled = false, notify = false }})
 require('lazy').setup({
   { import = 'plugins' },
   { import = 'plugins.lsp' },
   { import = 'plugins.debuggers' },
-  checker = { enabled = true, notify = false },
-  change_detection = { enabled = false, notify = false } -- do not notify me when dotfiles change
 })
