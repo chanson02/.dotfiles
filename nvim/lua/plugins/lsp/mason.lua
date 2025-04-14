956134a19e9f05e11d306bce8e2323767f1aba4b
Cooper Hanson <cooperhanson2020@gmail.com>
2025-04-14 17:14:16 -0500

    Removed lspconfig plugin

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -1,11 +1,11 @@
 --[[
 LSP Configuration:
-lspconfig is used to register individual servers to neovim
 mason is used to download servers
 mason-lspconfig automatically sets up the configuration for a server once it's been downloaded
 
 
 :h lsp-defaults
+:h mason-lspconfig-automatic-server-setup
 --]]
 
 -- Dependency for developing neovim
@@ -20,11 +20,8 @@ local lazydev = {
 }
 
 local deps = {
-  {
-    'williamboman/mason-lspconfig.nvim',
-    dependencies = { 'williamboman/mason.nvim', config = true }
-  },
-  { 'saghen/blink.cmp' },
+  { 'williamboman/mason.nvim', config = true },
+  { 'saghen/blink.cmp' },  -- configured in own file
   lazydev
 }
 
@@ -32,6 +29,7 @@ local diag_disable = { virtual_text = false, underline = false }
 local diag_enable  = { virtual_text = { source = true },  underline = true, sign = true }
 vim.diagnostic.config(diag_disable) -- default on startup
 vim.g.diagnostics_active = false
+
 -- Toggle vims diagnostics module. Useful for keeping a clean screen
 function _G.toggle_diagnostics()
   if vim.g.diagnostics_active
@@ -54,34 +52,16 @@ local function set_lsp_keymaps(args)
 end
 
 local function default_handler(server_name)
-  require('lspconfig')[server_name].setup({
-    capabilities = require('blink.cmp').get_lsp_capabilities()
-  })
-end
-
-local function lua_handler()
-  local lspconfig = require('lspconfig')
-  lspconfig.lua_ls.setup({
-    capabilities = require('blink.cmp').get_lsp_capabilities(),
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { "vim" }
-        }
-      }
-    }
-  })
+  vim.lsp.enable(server_name)
 end
 
 local lsp_handlers = {
    default_handler,
-   ["lua_ls"] = lua_handler,
    ['rust_analyzer'] = function() end, -- setup in rustaceanvim.lua
 }
 
 -- setup separately because not supported by mason
 local function setup_dart()
-  local lspconfig = require('lspconfig')
   local dart_path = vim.fn.expand('$HOME/fvm/default/bin/dart')
   if not file_exists(dart_path) then return end -- only setup dart if installed
 
@@ -90,23 +70,21 @@ local function setup_dart()
   local fvm_dart_path = root .. '/.fvm/flutter_sdk/bin/dart'
   if file_exists(fvm_dart_path) then dart_path = fvm_dart_path end
 
-  lspconfig.dartls.setup({
-    capabilities = require('blink.cmp').get_lsp_capabilities(),
+  vim.lsp.config['dartls'] = {
     cmd = { dart_path, 'language-server', '--protocol=lsp' },
     settings = {
       dart = {
         flutter = true
       }
     }
-  })
+  }
 end
 
 return {
-  "neovim/nvim-lspconfig",
+  "williamboman/mason-lspconfig.nvim",
   event = { 'BufReadPre', 'BufNewFile' },
   dependencies = deps,
   config = function()
-    require('mason').setup()
     require('mason-lspconfig').setup({ handlers = lsp_handlers })
     vim.api.nvim_create_autocmd('LspAttach', {
       callback = function(args)
