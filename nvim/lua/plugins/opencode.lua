bb34eaa393a4d7d6678e6f77d8b10932ad8afb52
Cooper Hanson <cooperhanson2020@gmail.com>
2026-07-03 19:03:40 -0500

    bump plugins

--- nvim/lua/plugins/opencode.lua
+++ nvim/lua/plugins/opencode.lua
@@ -1,5 +1,25 @@
 vim.pack.add({ 'https://github.com/nickjvandyke/opencode.nvim' })
 
-vim.keymap.set({ 'n', 't' }, '<C-.>', function()
-  require('opencode').toggle()
-end, { desc = 'Toggle opencode' })
+vim.g.opencode_opts = {
+}
+
+vim.o.autoread = true
+
+local k = vim.keymap.set
+k("n",          "<C-.>", function() require("opencode").ask() end, { desc = "Opencode" })
+k({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ") end, { desc = "Ask opencode" })
+k({ "n", "x" }, "<leader>os", function() require("opencode").select() end,       { desc = "Select opencode" })
+k({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Append range to OpenCode", expr = true })
+k("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Append line to OpenCode", expr = true })
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function(args)
+    local bufname = vim.api.nvim_buf_get_name(args.buf)
+    if bufname:match("opencode %-%-port") then
+      vim.keymap.set("n", "<C-u>", function() require("opencode").command("session.half.page.up") end, { buffer = args.buf })
+      vim.keymap.set("n", "<C-d>", function() require("opencode").command("session.half.page.down") end, { buffer = args.buf })
+      vim.keymap.set("t", "<C-u>", [[<Cmd>lua require("opencode").command("session.half.page.up")<CR>]], { buffer = args.buf })
+      vim.keymap.set("t", "<C-d>", [[<Cmd>lua require("opencode").command("session.half.page.down")<CR>]], { buffer = args.buf })
+    end
+  end
+})
