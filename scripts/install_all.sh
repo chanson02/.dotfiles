2105747d28dec78e97fbbec0270058d5e433a27d
Cooper Hanson <cooperhanson2020@gmail.com>
2026-09-02 17:37:58 -0500

    bugfix: OSC113 was breaking starship prompt

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -10,7 +10,7 @@ bash "$scripts_dir/install_calamp.sh"
 
 omarchy pkg add postgresql
 
-"$dotfiles/bash_tools/add_to_bashrc" "PROMPT_COMMAND='printf \"\\033]133;A\\007\"'"
+"$dotfiles/bash_tools/add_to_bashrc" 'PS0="${PS0}\033]133;A\007"'
 "$dotfiles/bash_tools/add_to_bashrc" 'source "$HOME/.config/opencode/opencode.sh"'
 
 curl https://mise.run | sh
